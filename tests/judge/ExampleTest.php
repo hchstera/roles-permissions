4e96e741893277657b62dd54f7266959e20529d2
Hchs <g9308370@hotmail.com>
2017-01-01 20:44:55 +0800

    update for basic test

---
+++ tests/judge/ExampleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use Hchs\Judge\BaseTestCase;
+
+class ExampleTest extends BaseTestCase
+{
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    public function testBasicExample()
+    {
+        $this->assertEquals(1, 1);
+    }
+}
